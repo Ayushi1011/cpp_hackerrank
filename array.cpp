ceff624e63d7287c6ce352ff3a8d17c34117297c
Ayushi Ahuja <70264477+Ayushi1011@users.noreply.github.com>
2021-01-11 21:15:04 +0530

    Create array.cpp

---
+++ array.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int i;
+    cin>>i;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+   int arr[i]; 
+    for(int j=0;j<i;j++) 
+     
+    cin>>arr[j];
+for(int j=i-1;j>=0;j--)
+    cout<<arr[j]<<" ";
+
+    
+        return 0;
+}
